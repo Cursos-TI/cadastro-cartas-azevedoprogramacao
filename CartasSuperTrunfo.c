06c547143a9cbfdd151e2a9e2fbfeb401f390cd8
azevedoprogramacao <azevedoguilherme106@gmail.com>
2026-03-26 14:23:24 +0000

    Apaguei tudo pra recomeçar do 0

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,24 +6,13 @@
 
 int main() {
   // Área para definição das variáveis para armazenar as propriedades das cidades
-      char estado = 'A';
-      char codigo[10] = "A01";
-      char cidade[15] = "Salvador";
-      int populacao = 2560000;
-      int pontosturisticos = 25;
-      float area = 693.83;
-      float pib = 76.700000000;
-
+    
   // Área para entrada de dados
-     scanf("Estado %c\n", estado);
-     scanf("Código: %s\n", &codigo);
-     scanf("Nome da cidade: %s\n", &cidade);
-     scanf("População: %d\n", populacao);
-     scanf("Área: %.2f km² \n", area);
-     scanf("PIB: %.3f bilhões de reais \n", pib);
-     scanf("Números de pontos turísticos: %d\n", pontosturisticos);
-     
+      
   // Área para exibição dos dados da cidade
+ 
+  
 
-return 0;
+
+
 } 
